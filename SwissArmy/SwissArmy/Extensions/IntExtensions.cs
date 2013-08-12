0f5b782d959a494b6d9e0ae772c2cc07e0b9687b
Jace <jace.rhea@gmail.com>
2013-08-12 16:26:29 -0500

    refactoring

--- SwissArmy/SwissArmy/Extensions/IntExtensions.cs
+++ SwissArmy/SwissArmy/Extensions/IntExtensions.cs
@@ -11,5 +11,10 @@
         {
             return value % 2 != 0;
         }
+
+        public static bool IsDivisibleBy(this int value, int divisor)
+        {
+            return value % divisor == 0;
+        }
     }
 }
