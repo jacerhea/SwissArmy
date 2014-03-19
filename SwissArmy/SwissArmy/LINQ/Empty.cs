1daac9abb0c1730004c071aff16ba1bfeee2e926
Jace <jace.rhea@gmail.com>
2014-03-18 20:27:40 -0500

    New unit tests.
    Empty implementation.

---
+++ SwissArmy/SwissArmy/LINQ/Empty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwissArmy.LINQ
+{
+    public  static partial class LinqExtended
+    {
+        public static bool Empty<T>(this IEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            var iterator = source.GetEnumerator();
+            return !iterator.MoveNext();
+        }
+
+        public static bool Empty<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            bool any = false;
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    any = true;
+                    break;
+                }
+            }
+            return !any;
+        }
+    }
+}
