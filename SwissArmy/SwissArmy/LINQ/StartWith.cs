2ebbcde8fef05f0d52030c141c72c215c8dbc680
Jace <jace.rhea@gmail.com>
2014-06-18 22:18:45 -0500

    created new linq methods startswith and starts at

---
+++ SwissArmy/SwissArmy/LINQ/StartWith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwissArmy.LINQ
+{
+    public static partial class LinqExtended
+    {
+        public static IEnumerable<TSource> StartWhen<TSource, T1>(this IEnumerable<TSource> source, Func<T1, bool> startWhen)
+        {
+            var started = false;
+            foreach (var item in source)
+            {
+                if (!started && item.Equals(startAt))
+                {
+                    started = true;
+                }
+                if (started)
+                {
+                    yield return item;
+                }
+            }
+        } 
+    }
+}
