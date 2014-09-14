a8a33de6f1d453dd98c94c7e1bc21f212444a392
Jace <jace.rhea@gmail.com>
2014-09-14 13:18:10 -0500

    sending testfixture.

--- SwissArmy.Test/LINQ/MedianTestFixture.cs
+++ SwissArmy.Test/LINQ/MedianTestFixture.cs
@@ -32,6 +32,7 @@ namespace SwissArmy.Test.LINQ
             var source = new List<int> { -10, 9, 8, 7, 6, 5, -4, 3, 2, -1 };
             var result = source.Mean();
             Assert.AreEqual(2.5, result);
+            Assert.AreEqual(2.5, result);
         }
     }
 }
